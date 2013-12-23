912976b7aa127b74b6f9eabd484f7d8bebbba9ad
Thomas Wöhlke <thomas@woehlke.org>
2013-12-23 08:52:45 +0100

    use case new address

--- src/main/java/org/woehlke/addresslist/web/AddressBean.java
+++ src/main/java/org/woehlke/addresslist/web/AddressBean.java
@@ -1,54 +1,68 @@
 package org.woehlke.addresslist.web;
 
-import org.woehlke.addresslist.entities.Address;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
  * User: tw
- * Date: 22.12.13
- * Time: 15:45
+ * Date: 23.12.13
+ * Time: 08:44
  * To change this template use File | Settings | File Templates.
  */
 @ManagedBean
 @SessionScoped
 public class AddressBean implements Serializable {
 
-    public List<Address> getAddressList(){
-        List<Address> list = new ArrayList<Address>();
-        Address a1 = new Address();
-        a1.setName("Willy Wombel");
-        a1.setStreet("Kastanienallee");
-        a1.setHouseNumber("17");
-        a1.setZip("44801");
-        a1.setCity("Bochum");
-        a1.setId(1L);
-        Address a2 = new Address();
-        a2.setName("Honk Waffelo");
-        a2.setStreet("Eichenstr.");
-        a2.setHouseNumber("31");
-        a2.setZip("44581");
-        a2.setCity("Castrop-Rauxel");
-        a2.setId(2L);
-        Address a3 = new Address();
-        a3.setName("Willy Wonka");
-        a3.setStreet("An der Schokoladenfabrik");
-        a3.setHouseNumber("42");
-        a3.setZip("12345");
-        a3.setCity("Berlin");
-        a3.setId(3L);
-        list.add(a1);
-        list.add(a2);
-        list.add(a3);
-        return list;
+    //Todo: Validation
+    private String name;
+    private String street;
+    private String houseNumber;
+    private String zip;
+    private String city;
+
+    public String getName() {
+        return name;
     }
 
-    public String newAdressForm(){
-        return "new.xhtml";
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String save(){
+        return "hello.xhtml";
     }
 }
